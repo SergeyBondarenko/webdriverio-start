87ac3a17ef84e049a877a4accd6934a5bc934bfe
SergeyBondarenko <sergibondarenko@gmail.com>
2016-05-18 17:21:58 +0200

    Add chrome

--- wdio.conf.js
+++ wdio.conf.js
@@ -45,6 +45,8 @@ exports.config = {
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: [{
+      browserName: 'chrome'
+    },{
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instance available you can make sure that not more than
         // 5 instance gets started at a time.
